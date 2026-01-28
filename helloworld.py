0bb6c76b8d7987475c14207c025a14ef222345c3
Javier Martinez <javier.martinezperches01@gmail.com>
2026-01-28 15:01:12 -0600

    feat: ejercicios del dia 1 y creado el folder

---
+++ helloworld.py
@@ -0,0 +1,44 @@
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+# Excercise: Level 1
+
+print(4+3)
+print(4-3)
+print(4*3)
+print(4%3)
+print(4/3)
+print(4**3)
+print(4//3)
+
+print("Javi")
+print('Family name')
+print('Mexico')
+print('I am enjoying 30 days of python')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Javi'))
+print(type('Family Name'))
+print(type('Mexico'))
