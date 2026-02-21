a184358dac1042cb548a82429fd705c127508f6b
Javier Martinez <javier.martinezperches01@gmail.com>
2026-02-20 23:14:32 -0600

    feat: lei la teoria, pendiente hacer el ejercicio

---
+++ day_3/excercise_day_3.py
@@ -0,0 +1,15 @@
+age = 27
+height = 1.76
+complicated = 10j
+
+print(type(height))
+print(type(complicated))
+
+#script that prompts the user to enter base and height of a traingle and calculate an area of this triangle (0.5 * b* n)
+
+base = input('write the base')
+height = input('write the height')
+
+area_of_traingle = base * height * .5
+
+print('The area of traingle is: ', area_of_traingle )
