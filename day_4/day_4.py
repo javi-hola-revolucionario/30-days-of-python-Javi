77ca773530e75a67151fe4f5ebff5cdca062304d
Javier Martinez <javier.martinezperches01@gmail.com>
2026-02-22 13:22:46 -0600

    Feat: avanzamos hasta slicing python string teoria

---
+++ day_4/day_4.py
@@ -0,0 +1,158 @@
+letter = 'P'                # A string could be a single character or a bunch of texts
+print(letter)               # P
+print(len(letter))          # 1
+greeting = 'Hello, World!'  # String could be made using a single or double quote,"Hello, World!"
+print(greeting)             # Hello, World!
+print(len(greeting))        # 13
+sentence = "I hope you are enjoying 30 days of Python Challenge"
+print(sentence)
+print('------------------------------------------------------------')
+multiline_string = '''I am a teacher and enjoy teaching. 
+I didn't find anything as rewarding as empowering people. 
+That is why I created 30 days of python.'''
+print(multiline_string)
+# Another way of doing the same thing
+multiline_string = """I am a teacher and enjoy teaching.
+I didn't find anything as rewarding as empowering people.
+That is why I created 30 days of python."""
+print(multiline_string)
+print('------------------------------------------------------------')
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+space = ' '
+full_name = first_name  +  space + last_name
+print(full_name) # Asabeneh Yetayeh
+# Checking the length of a string using len() built-in function
+print(len(first_name))  # 8
+print(len(last_name))   # 7
+print(len(first_name) > len(last_name)) # True
+print(len(full_name)) # 16
+
+# In Python and other programming languages \ followed by a character is an escape sequence. 
+# Let us see the most common escape characters
+
+# \n: new line
+# \t: Tab means (8 spaces)
+# \\: Back slash
+# \': single quote (')
+# \": double quote (")
+
+print ('I hope everyone is enjoying the Python Challenge. \n Are you?') #line break
+print('Days\tTopics\tExcercises') # adding tab space or 4 spaces
+print('Day 1\t5\t5')
+print('Day 2\t6\t20')
+print('Day 3\t5\t23')
+print('Day 4\t1\t35')
+print('This is a backslah symbol (\\)') #to write a backslash
+print('In every programming language it starts with \"Hello, World\"') # to write a double quote inside a single quote
+print('------------------------------------------------------------')
+
+# The "%" operator is used to format a set of variables enclosed in a "tuple" (a fixed size list), together with a format string
+# %s - String (or any object with a string representation, like numbers)
+# %d - Integers
+# %f - Floating point numbers
+# "%.number of digitsf" - Floating point numbers with fixed precision
+#----------------------------------------------------------------------------
+# Strings only
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+language = 'Python'
+formated_string = 'I am %s %s. I teach %s' %(first_name, last_name, language)
+print(formated_string)
+print('------------------------------------------------------------')
+# Strings  and numbers
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formated_string = 'The area of circle with a radius %d is %.2f.' %(radius, area) # 2 refers the 2 significant digits after the point
+print(formated_string)
+print('------------------------------------------------------------')
+python_libraries = ['Django', 'Flask', 'NumPy', 'Matplotlib','Pandas']
+formated_string = 'The following are python libraries:%s' % (python_libraries)
+print(formated_string) # "The following are python libraries:['Django', 'Flask', 'NumPy', 'Matplotlib','Pandas']"
+print('------------------------------------------------------------')
+
+# New format
+
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+language = 'Python'
+formated_string = 'I am {} {}. I teach {}'.format(first_name, last_name, language)
+
+print(formated_string)
+a = 4
+b = 3
+print('{} + {} = {}'.format(a, b, a + b))
+print('{} - {} = {}'.format(a, b, a - b))
+print('{} * {} = {}'.format(a, b, a * b))
+print('{} / {} = {:.2f}'.format(a, b, a / b)) # limits it to two digits after decimal
+print('{} % {} = {}'.format(a, b, a % b))
+print('{} // {} = {}'.format(a, b, a // b))
+print('{} ** {} = {}'.format(a, b, a ** b))
+
+# output
+# 4 + 3 = 7
+# 4 - 3 = 1
+# 4 * 3 = 12
+# 4 / 3 = 1.33
+# 4 % 3 = 1
+# 4 // 3 = 1
+# 4 ** 3 = 64
+
+# Strings  and numbers
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formated_string = 'The area of a circle with a radius {} is {:.2f}.'.format(radius, area) # 2 digits after decimal
+print(formated_string)
+print('------------------------------------------------------------')
+print('------------------------------------------------------------')
+
+#String interpolation / f-String (Python 3.6+)
+
+# Another new string formatting is string interpolation, f-strings. 
+# Strings start with f and we can inject the data in their corresponding positions.
+a = 4
+b = 3
+print(f'{a} + {b} = {a +b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
+print(f'{a} / {b} = {a / b:.2f}')
+print(f'{a} % {b} = {a % b}')
+print(f'{a} // {b} = {a // b}')
+print(f'{a} ** {b} = {a ** b}')
+print('------------------------------------------------------------')
+print('------------------------------------------------------------')
+
+print('Un packing characters')
+language = 'Python'
+a,b,c,d,e,f = language # unpacking sequence characters into variables
+print(a) # P
+print(b) # y
+print(c) # t
+print(d) # h
+print(e) # o
+print(f) # n
+
+print('another way of doing the same thing but simpler')
+
+language = 'Hola' #es python en vez de esa variable
+first_letter = language[0]
+print(first_letter) # P
+second_letter = language[1]
+print(second_letter) # y
+last_index = len(language) - 1
+last_letter = language[last_index]
+print(last_letter) # n
+
+print('If we want to start from right end we can use negative indexing. -1 is the last index.')
+
+language = 'Hello' #es python en vez de esa variable
+last_letter = language[-1]
+print(last_letter) # n
+second_last = language[-2]
+print(second_last) # o
+print('------------------------------------------------------------')
+print('------------------------------------------------------------')
+
+# Slicing Python Strings aqui me quede.
