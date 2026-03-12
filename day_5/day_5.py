66a79c1f5275c49908d3a1e2220c3b4c5cb6c527
Javier Martinez <javier.martinezperches01@gmail.com>
2026-03-11 20:57:09 -0600

    Dia 5, listas llegamos a la parte de como acceder a cada variable dentro de []

---
+++ day_5/day_5.py
@@ -0,0 +1,32 @@
+#Day 5 Lists
+
+# There are four collection of data types in Python: 
+
+# List, is a collection which is ordered and changeable. Allows duplicate members.
+
+# Tuple: is a collection which is ordered and unchangeable or unmodifiable (immutable). Allow duplicate members.
+
+# Set: is a collection which is unordered, un-indexed and unmodifiable, but 
+# we can add new items to the set. Duplicate memebers are not allowed. 
+
+# Dictionary: is a collection which is unordered, changeable (modifiable) and indexed. No duplicate members.
+
+# syntax for list
+
+lst = list() # creates an empty list
+lst = [] # this is an empty list, no item in the list
+
+fruits = ['banana', 'orange', 'mango', 'lemon']                     # list of fruits
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']      # list of vegetables
+animal_products = ['milk', 'meat', 'butter', 'yoghurt']             # list of animal products
+web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB'] # list of web technologies
+
+
+print('I hate fruits: ', fruits)
+
+# A list can have items of different data types
+
+lst =['Javier',29, True, {'pais': 'Mexico', 'cuidad': 'Monterrey'}]
+print(lst)
+
+# We can access a list through its index. We can also use the negative for starting from the end.
