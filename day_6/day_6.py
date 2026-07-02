2305e22b5b4f6406150de415ec6fd8a97867d674
Javier Martinez <javier.martinezperches01@gmail.com>
2026-07-01 21:27:01 -0600

    day 6 advancement

---
+++ day_6/day_6.py
@@ -0,0 +1,14 @@
+#Day_6_ Tupples
+
+#A tupple is a collection of different data types which is ordered and unchangeable, cannot add, insert, remove methods.
+
+# syntax
+empty_tuple = ()
+# or using the tuple constructor
+empty_tuple = tuple()
+
+tpl = (1, 2, 3, 4, 5)
+#to access a tupple we use the index, just like lists
+first_item = tpl[0]
+
+#slicing tupples
